1a98aa8977bb140d90d9a7c42d6cc3f9e4490f5d
Anthony Car <agcarat@gmail.com>
2016-05-19 23:35:59 -0400

    added .gitignore

---
+++ bench.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "thread_processor.hpp"
+int k;
+
+void func(int i) {
+    int j = i;
+    for(int z=0; z< 1000; z++ ){
+        j+=z;
+    }
+    k +=j;
+}
+int main(int argc, char * argv[]) {
+	ThreadProcessor testProcessor(10);
+
+	BatchTracker jq(&testProcessor);
+    for (int i=0; i< atoi(argv[1]); i++) {
+        jq.post(boost::bind(&func, i));
+    }
+	jq.wait_until_done();
+    return EXIT_SUCCESS;
+}
